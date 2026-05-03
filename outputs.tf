6f1b3f8696be7f2a5c2eb43c76e20a6652ce1983
solivalle <127651078+solivalle@users.noreply.github.com>
2026-05-03 17:02:58 -0600

    Terraform fmt fixes

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,4 @@
 output "bucket_name" {
- description = "Name of the provisioned S3 bucket"
- value       = aws_s3_bucket.main.bucket
+  description = "Name of the provisioned S3 bucket"
+  value       = aws_s3_bucket.main.bucket
 }
